dc40f3a520e1683eef6cc4313b51ecaa6b82ac9a
Adan Silva <ev3mcdesigner@gmail.com>
2020-02-10 14:21:12 -0800

    Add Joysticks to Robot header file

--- src/main/include/Robot.hpp
+++ src/main/include/Robot.hpp
@@ -2,9 +2,18 @@
 
 #pragma once
 
+#include <cscore.h>
+#include <frc/DriverStation.h>
+#include <frc/Joystick.h>
+#include <frc/PowerDistributionPanel.h>
+#include <frc/livewindow/LiveWindow.h>
 #include <frc/TimedRobot.h>
 
 #include "Constants.hpp"
+#include "subsystems/Drivetrain.hpp"
+#include "subsystems/Flywheel.hpp"
+#include "subsystems/Intake.hpp"
+#include "subsystems/Turret.hpp"
 
 namespace frc3512 {
 
@@ -23,6 +32,12 @@ public:
     void TeleopPeriodic() override;
 
 private:
+
+    frc::Joystick m_driveStick1{kDriveStick1Port};
+    frc::Joystick m_driveStick2{kDriveStick2Port};
+    frc::Joystick m_appendageStick{kAppendageStickPort};
+    frc::Joystick m_appendageStick2{kAppendageStick2Port};
+
 };
 
 }  // namespace frc3512
